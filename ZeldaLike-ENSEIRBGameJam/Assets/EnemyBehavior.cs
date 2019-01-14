21408bdc5831274b6416fb45958f915029a53353
Adrionche <adrionche@gmail.com>
2019-01-14 19:31:05 +0100

    bite la vengeance

--- ZeldaLike-ENSEIRBGameJam/Assets/EnemyBehavior.cs
+++ ZeldaLike-ENSEIRBGameJam/Assets/EnemyBehavior.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Combat : MonoBehaviour {
+public class EnemyBehavior : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject m_fireBall = null;
+    private int shoot_time = 0;
+    public int hp = 10;
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (hp == 0)
+        {
+            Destroy(gameObject);
+        }
+        shoot_time ++;
+        if (shoot_time % 30 == 0)
+        {
+            ShootFireball();
+        }
+    }
+    
+    // Creates a fireball, and launches it
+    private void ShootFireball()
+    {
+        GameObject newFireball = Instantiate(m_fireBall, this.transform) as GameObject;
+
+        newFireball.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+
+        FireBehavior fireBallBehavior = newFireball.GetComponent<FireBehavior>();
+
+
+        GameObject player = GameObject.Find("Player");
+
+        Vector3 v = (player.transform.position - this.transform.position).normalized;
+
+        Vector2 dir = v;
+
+        if (fireBallBehavior != null)
+        {
+            fireBallBehavior.Launch(dir);
+        }
+    }
 }
--- ZeldaLike-ENSEIRBGameJam/Assets/LightningBehavior.cs
+++ ZeldaLike-ENSEIRBGameJam/Assets/LightningBehavior.cs
@@ -8,7 +8,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireBehavior : MonoBehaviour {
+public class LightningBehavior : MonoBehaviour
+{
     Rigidbody2D m_rb2D;
 
     float m_launchedTime; // Absolute time (in sec.) when the fireball has been launched
@@ -16,7 +17,8 @@ public class FireBehavior : MonoBehaviour {
 
     float m_speed = 100f; // Speed of the fireball
 
-    void Awake () {
+    void Awake()
+    {
         m_rb2D = gameObject.GetComponent<Rigidbody2D>();
         m_launchedTime = Time.realtimeSinceStartup;
     }
@@ -25,24 +27,30 @@ public class FireBehavior : MonoBehaviour {
     {
         m_rb2D.AddForce(direction.normalized * m_speed, ForceMode2D.Impulse);
     }
-	
-	void Update () {
+
+    void Update()
+    {
         // Checks if the fireball should remain on screen
         // or if the life time has been reached
-		if (Time.realtimeSinceStartup > m_launchedTime + m_fireDuration)
+        if (Time.realtimeSinceStartup > m_launchedTime + m_fireDuration)
         {
             Destroy(gameObject);
         }
-	}
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // Destroys the fireball when it hits something, except the player or another fireball
         // (to prevent the fireball to be destroyed as soon as it is created)
-        if (collision.gameObject.tag != "Fireball" && collision.gameObject.tag != "Enemy")
+        /*if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<EnemyBehavior>().hp--;
+        }*/
+        if (collision.gameObject.tag != "Fireball" && collision.gameObject.tag != "Player")
         {
             Destroy(gameObject);
-        } else
+        }
+        else
         {
             Physics2D.IgnoreCollision(this.GetComponent<Collider2D>(), collision.gameObject.GetComponent<Collider2D>());
         }
