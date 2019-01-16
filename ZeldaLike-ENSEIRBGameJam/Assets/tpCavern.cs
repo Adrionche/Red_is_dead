291132eeeaa92c3e78cfb2530c148b41e4c57d83
Anatole <lindien.martin@gmail.com>
2019-01-16 19:39:55 +0100

    caverne ok

---
+++ ZeldaLike-ENSEIRBGameJam/Assets/tpCavern.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tpCavern : MonoBehaviour {
+
+    public GameObject Player;
+    public GameObject Syst;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player.transform.position = new Vector3(2760, 859, 0);
+        Syst.GetComponent<World_change>().enabled = false;
+    }
+}
---
+++ ZeldaLike-ENSEIRBGameJam/Assets/triggerchange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerchange : MonoBehaviour {
+
+    public GameObject Syst;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Syst.GetComponent<World_change>().enabled = true;
+    }
+}
