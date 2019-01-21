d685d9da35898a3dd401bf8787f302953b4a3836
natoles <lindien.martin@gmail.com>
2019-01-21 16:48:47 +0100

    dialoque debut

---
+++ ZeldaLike-ENSEIRBGameJam/Assets/StartDial.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartDial : MonoBehaviour {
+
+    public List<DialogPage> Dial;
+    public GameObject Player;
+    // Use this for initialization
+    void Start () {
+        Player.GetComponent<PlayerBehavior>().m_dialogDisplayer.SetDialog(Dial);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
